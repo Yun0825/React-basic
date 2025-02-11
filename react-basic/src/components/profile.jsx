cfecb7fd22e4626447d2fd3f7c96f7d13c225f13
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-02-11 23:54:33 +0900

    Props 사용해보기

---
+++ react-basic/src/components/profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function profile({image, name, title}) {
+  return (
+    <div className='profile'>
+        <img
+        className='photo'
+        src={image}
+        alt="avatar" />
+        <h1>{name}</h1>
+        <p>{title}</p>
+    </div>
+  )
+}
