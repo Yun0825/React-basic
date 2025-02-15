57f68fc581f01dadba8b7da9e3d90d067f040825
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-02-16 00:23:01 +0900

    useReducer 사용

--- react-basic/src/AppMentor.jsx
+++ react-basic/src/AppMentor.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
+import personReducer from './reducer/person-reducer';
 
 export default function AppMentor() {
-  const [person, setPerson] = useState({
-    name: '엘리',
-    title: '개발자',
-    mentors: [{
-      name: '밥',
-      title: '시니어개발자',
-    },
-    {
-      name: '제임스',
-      title: '시니어개발자',
-    },
-  ]});
+  //const [person, setPerson] = useState(initialPerson);
+  const [person, dispatch]= useReducer(personReducer, initialPerson)
+
+  const handleUpdate = () => {
+    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`)
+    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`)
+    dispatch({type: 'updated', prev, current})
+  }
+  
+  const handleAdd = () => {
+    const name = prompt(`멘토의 이름을 입력해주세요`)
+    const title = prompt(`멘토의 Title을 입력해주세요`)
+    dispatch({type: 'added', name, title})
+  }
+
+  const handleDelete = () => {
+    const name = prompt(`누구를 삭제하고 싶은가요?`)
+    dispatch({type: 'deleted', name})
+  }
 
   return (
     <div>
@@ -27,22 +35,32 @@ export default function AppMentor() {
         ))}
       </ul>
       <button
-        onClick={() => {
-          const prevName = prompt(`누구의 이름을 바꾸고 싶은가요?`)
-          const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`)
-          setPerson(prev => ({
-            ...prev, mentors: prev.mentors.map(mentor => {
-                if(mentor.name === prevName){
-                    return {...mentor, name:current}
-                }
-
-                return mentor;
-            })
-          }))
-        }}
+        onClick={handleUpdate}
       >
         멘토 이름 바꾸기
+      </button>
+      
+      <button onClick={handleAdd}>
+        멘토 추가하기
+      </button>
+
+      <button onClick={handleDelete}>
+        멘토 삭제하기
       </button>
     </div>
   );
 }
+
+
+const initialPerson = {
+    name: '엘리',
+    title: '개발자',
+    mentors: [{
+      name: '밥',
+      title: '시니어개발자',
+    },
+    {
+      name: '제임스',
+      title: '시니어개발자',
+    },
+  ]}
